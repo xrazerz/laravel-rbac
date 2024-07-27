289f065d29ad5bb18695fb8063e2459b7c527c4f
xrazerz <xrazerz.dev@gmail.com>
2024-07-28 02:03:55 +0300

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'xrazerz/laravel-rbac',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd7183fb9cc9bb8cec95716f31d137f065c7078a0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'xrazerz/laravel-rbac' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd7183fb9cc9bb8cec95716f31d137f065c7078a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
